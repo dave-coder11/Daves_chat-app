4db30108bfb67088857d01b9edc70fa3865624cd
dave-coder11 <106849369+dave-coder11@users.noreply.github.com>
2022-07-22 07:01:07 -0400

    ahhhhhhhhhhhhhhahhahahhahahahahhaaaahahhahhhhhhh

--- Voice chat/Voice_chat_client.py
+++ Voice chat/Voice_chat_client.py
@@ -4,13 +4,19 @@ import threading
 import keyboard
 
 while True:
-    if keyboard.is_pressed('t'):  # if key t is pressed 
-        receiver = AudioReceiver('192.168.219.30', 9999)
-        receive_thread = threading.Thread(target=receiver.start_server)
-    else:
-        sender = AudioSender('192.168.219.30', 9999)
-        sender_thread = threading.Thread(target=sender.start_stream)
+    try:
+        if keyboard.is_pressed('t'):  # if key t is pressed 
+            receiver = AudioReceiver('192.168.219.30', 9999)
+            receive_thread = threading.Thread(target=receiver.start_server)
+        else:
+            sender = AudioSender('192.168.219.30', 9999)
+            sender_thread = threading.Thread(target=sender.start_stream)
+    except:
+        print('something whent wrong pleas try later')
+        break
+            
     try:
         sender_thread.start()
     except:
         print("the client disconected")
+        break
