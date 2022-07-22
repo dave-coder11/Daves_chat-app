ab99afdabee15f69bc193719e732a6e36db57d5b
dave-coder11 <106849369+dave-coder11@users.noreply.github.com>
2022-07-22 07:02:02 -0400

    got rid of while loop

--- Voice chat/Voice_chat_server.py
+++ Voice chat/Voice_chat_server.py
@@ -3,10 +3,10 @@ from vidstream import AudioReceiver
 import threading
 import keyboard
 
-while True:
-    sender = AudioSender('192.168.219.30', 9999)
-    sender_thread = threading.Thread(target=sender.start_stream)
-    try:
-        sender_thread.start()
-    except:
-        print("the client disconected")
+
+sender = AudioSender('192.168.219.30', 9999)
+sender_thread = threading.Thread(target=sender.start_stream)
+try:
+    sender_thread.start()
+except:
+    print("the client disconected")
